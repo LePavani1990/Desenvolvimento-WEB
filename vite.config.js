a997b7213cec50bfe3cc954e4117e7b797ab11a5
Lepavani <lepavani1990@gmail.com>
2024-02-22 18:00:07 -0300

    configurações de deploy

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,8 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: "/Desenvolvimento-WEB/", // Altere para corresponder ao seu subdiretório, se aplicável
   plugins: [react()],
+  base: "/Desenvolvimento-WEB",
   resolve: {
     alias: {
       extensions: [".js", ".jsx"],
